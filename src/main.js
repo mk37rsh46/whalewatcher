c2ccd7fc1688a2193cf4fe48b18b94d49e889a8b
RemSleep <keshav.1.rao@gmail.com>
2020-07-25 12:27:47 -0700

    implemented using vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import AsyncComputed from 'vue-async-computed'
 import {BadgerAccordion, BadgerAccordionItem} from 'vue-badger-accordion'
 import vuetify from './plugins/vuetify';
+import store from './store'
 Vue.use(AsyncComputed)
 Vue.component('BadgerAccordion', BadgerAccordion)
 Vue.component('BadgerAccordionItem', BadgerAccordionItem)
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
